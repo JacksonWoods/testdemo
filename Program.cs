f0cf2dfe039661235209394496ba5899063c8957
Jackson Woods <JacksonWoods@users.noreply.github.com>
2017-12-13 16:00:09 -0800

    Created new Program.cs file
    This will hold our sweet new app.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+```c#
+using Sytem;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillerApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ReadLine();
+    }
+  }
+
+}
